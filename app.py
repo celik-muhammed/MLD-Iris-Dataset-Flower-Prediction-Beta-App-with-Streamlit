7e37467e94e5288f6be5b51ca3ba1f974ebfc2a2
Muhammed ÇELİK <94930605+celik-muhammed@users.noreply.github.com>
2022-10-01 02:20:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,32 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
 import pickle
+
 model = pickle.load(open("final_model_iris", "rb"))
 
-st.title("the app prediction iris flower dataset")
-# st.success('This is a success message!')
-# st.info('This is a purely informational message')
-# st.error('This is an error')
+st.markdown("<h2 style='text-align:center; color:floralWhite;'>Iris Dataset Flower Prediction Beta App</h2>", unsafe_allow_html=True)
 
-st.sidebar.header("User input parameter")
+col1, col2, col3 = st.columns([1,8,1]) 
+#Image
+try:
+    #image url
+    url = "https://editor.analyticsvidhya.com/uploads/51518iris%20img1.png"
+    
+    with col2:
+        st.image(url, caption="Predicting the Prices of Cars")
+    
+except:
+    # Executed if error in the 
+    st.text("Image Not Loading!")
+    components.html('''
+    <script>
+        alert("Image Not Loading!");
+    </script>
+    ''')        
+
 
 def user_input_data():
-
     sepal_length=st.sidebar.slider('sepal lengrh1',4.3,7.9,5.4)
     sepal_width=st.sidebar.slider(' sepal width',2.0,4.4,3.4)
     petal_length=st.sidebar.slider('petal length',1.0,6.0,1.3)
@@ -27,6 +40,7 @@ def user_input_data():
     features=pd.DataFrame(data,index=[0])
     return features
 
+st.sidebar.header("User input parameter")
 input_df = user_input_data()
 
 if st.button("Predict"): 
